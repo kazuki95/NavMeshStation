617c9379c5e6ac9f55a81827935b8bbed26d8b65
kazuki95 <feryamagata123@gmail.com>
2021-04-22 12:47:41 -0300

    Initial commit

---
+++ Assets/Script/AgenteManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgenteManager : MonoBehaviour
+{ 
+    GameObject[] agents;// Use thisfor initializationvoid
+   void Start () 
+   {
+        agents= GameObject.FindGameObjectsWithTag("AI");
+   }
+    // Update is called once per framevoid
+   void Update () 
+   {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200))
+                {
+                    foreach(GameObject a in agents)
+                    a.GetComponent<AiControl>().agent.SetDestination(hit.point);
+            
+                }
+        }
+    }
+}
